e98cf72ecaa0175e3f4f528776fa8eac3dba559a
Denis <denis.shagim@icloud.com>
2025-01-03 01:03:22 +0500

    feat: add function for game

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-let secretNum = Math.ceil(Math.random() * 10);
+const secretNum = Math.ceil(Math.random() * 10);
 let tries = 0;
 
-function guessNum(num) {}
+function guessNum(num) {
+  if (tries >= 5) return alert("Игра окончена");
+  if (num === secretNum) {
+    alert("Вы угадали");
+    tries = 5;
+  } else {
+    alert("Вы не угадали");
+    tries++;
+  }
+}
